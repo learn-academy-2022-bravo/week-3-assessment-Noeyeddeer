29c491306c70e9c5f9b875e48680481cc83dd861
Noeyeddeer <john.e.mades@gmail.com>
2022-04-10 19:31:44 -0700

    initial commit

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,11 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+// const { it, expect } = require("@jest/globals")
+// const { describe } = require("yargs")
+
+// const { it, expect } = require("@jest/globals")
+// const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -15,6 +21,38 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
+describe("fibonacciGen", () => {
+    it("returns a fibonacci sequence of a given length based on input", () => {
+      expect(fibonacciGen(6)).toEqual([1, 1, 2, 3, 5, 8]) 
+      expect(fibonacciGen(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+})
+
+// ---Initial Jest Test Result---
+
+// FAIL  ./code-challenges.test.js
+// fibonacciGen
+//   ✕ returns a fibonacci sequence of a given length based on input
+
+// ● fibonacciGen › returns a fibonacci sequence of a given length based on input
+
+//   ReferenceError: fibonacciGen is not defined
+
+//     18 | describe("fibonacciGen", () => {
+//     19 |     it("returns a fibonacci sequence of a given length based on input", () => {
+//   > 20 |       expect(fibonacciGen(6)).toEqual([1, 1, 2, 3, 5, 8]) 
+//        |       ^
+//     21 |       expect(fibonacciGen(10)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+//     22 |     })
+//     23 | })
+
+//     at Object.<anonymous> (code-challenges.test.js:20:7)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+// Snapshots:   0 total
+// Time:        0.143 s
+
 // Example input: 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
@@ -24,6 +62,23 @@
 
 // b) Create the function that makes the test pass.
 
+// Create a function called fibonacciGen with a parameter called num
+// Declare an empty array, set first two indexes of array to one
+// Write for loop that starts at the second index and runs as long as the index is less than the argument passed into the function
+// Push numbers into the array which are a result of the previous two indexes being added together
+// Return array and call function
+const fibonacciGen = (num) => {
+  let arr = []
+  arr[0] = 1
+  arr[1] = 1
+  for (let i = 2; i < num; i++){
+    arr.push(arr[i - 2] + arr[i - 1])
+  }
+  return arr
+}
+
+fibonacciGen(6)
+fibonacciGen(10)
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
@@ -35,13 +90,90 @@ const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
+describe("onlyOddSort", () => {
+    it("returns a sorted array of only odd numbers based on input array", () => {
+        expect(onlyOddSort(fullArr1)).toEqual([-9, 7, 9, 199])
+        expect(onlyOddSort(fullArr2)).toEqual([-823, 7, 23])
+    })
+})
+
+// ---Initial Jest Test Result---
+
+// FAIL  ./code-challenges.test.js
+// onlyOddSort
+//   ✕ returns a sorted array of only odd numbers based on input array
+
+// ● onlyOddSort › returns a sorted array of only odd numbers based on input array
+
+//   ReferenceError: onlyOddSort is not defined
+
+//     85 | describe("onlyOddSort", () => {
+//     86 |     it("returns a sorted array of only odd numbers based on input array", () => {
+//   > 87 |         expect(onlyOddSort(fullArr1)).toEqual([-9, 7, 9, 199])
+//        |         ^
+//     88 |         expect(onlyOddSort(fullArr2)).toEqual([-823, 7, 23])
+//     89 |     })
+//     90 | })
+
+//     at Object.<anonymous> (code-challenges.test.js:87:9)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+// Snapshots:   0 total
+// Time:        0.143 s, estimated 1 s
 
 // b) Create the function that makes the test pass.
 
+// Create a function called onlyOddSort with a parameter called array
+// Create variable which is equal to the argument array filtered for typeof
+// Create another variable equal to that filtered array which filters the array further into only odd numbers
+// Return that array sorted so that the numbers will appear low to high
+// Call the function
+const onlyOddSort = (array) => {
+    arr1 = array.filter(value => typeof value === 'number')
+    arr2 = arr1.filter(value => value % 2 !== 0)
+    return arr2.sort((a, b) => a - b)
+}
+
+onlyOddSort(fullArr1)
+onlyOddSort(fullArr2)
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("accumSum", () => {
+    it("returns accumulating sum array based on input array", () => {
+        expect(accumSum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+        expect(accumSum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+        expect(accumSum(numbersToAdd3)).toEqual([])
+    })
+})
+
+// ---Initial Jest Test Result---
+
+// FAIL  ./code-challenges.test.js
+// accumSum
+//   ✕ returns accumulating sum array based on input array (1 ms)
+
+// ● accumSum › returns accumulating sum array based on input array
+
+//   ReferenceError: accumSum is not defined
+
+//     135 | describe("accumSum", () => {
+//     136 |     it("returns accumulating sum array based on input array", () => {
+//   > 137 |         expect(accumSum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+//         |         ^
+//     138 |         expect(accumSum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+//     139 |         expect(accumSum(numbersToAdd3)).toEqual([])
+//     140 |     })
+
+//     at Object.<anonymous> (code-challenges.test.js:137:9)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+// Snapshots:   0 total
+// Time:        0.126 s
 
 const numbersToAdd1 = [2, 4, 45, 9]
 // Excpected output: [2, 6, 51, 60]
@@ -54,3 +186,25 @@ const numbersToAdd3 = []
 
 
 // b) Create the function that makes the test pass.
+
+// Create a function called accumSum with an a parameter called array
+// Declare an empty array
+// Loop through the argument array with forEach function 
+// Use conditional statement to say that the first index should be left alone and all other indexes should be value at the current index plus the value at the previous index in the new array
+// Return the new array
+// Call the function
+const accumSum = (array) => {
+    let arr = []
+    array.forEach((num, index) => {
+        if (index === 0){
+            arr[index] = num
+        } else {
+            arr[index] = num + arr[index - 1]
+        }
+    })
+    return arr
+}
+
+accumSum(numbersToAdd1)
+accumSum(numbersToAdd2)
+accumSum(numbersToAdd3)
--- interview-questions.md
+++ interview-questions.md
@@ -9,59 +9,59 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: 'this' is a designator used inside of an object or class in order to refer to the class or object one is inside of when calling a method or key in a key:value pair.
 
-  Researched answer:
+  Researched answer: I was sort of correct. It appears that 'this' refers to an object and when used inside of an object, it refers to the object itself. Outside of an object it either refers to the global object or is undefined.
 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: React is a Javascript library which contains code necessary for building and combining components. Compenents are pieces of code in React which can be built to display HTML and run JSX commands on an HTML page. You can use React to build websites or apps which are both scalable and modular due to the various components used in their construction.
 
-  Researched answer:
+  Researched answer: React is not only a library, but a free and open-source front-end Javascript library. It is based on components and can be used to build single-page, mobile, or server-rendered apps.
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer: Drawing a blank on this one.
 
-  Researched answer:
+  Researched answer: The constructor() method is required in a React class component because constructor handles state initialization and event handlers, which all affect how the component is rendered on the Document Object Model.
 
 
 
 4. What is JSX? What is one notable difference between HTML and JSX?
 
-  Your answer:
+  Your answer: JSX is a fusion of HTML and Javascript which React uses to render components to the DOM.
 
-  Researched answer:
+  Researched answer: Javascript XML (JSX) allows the programmer to write HTML in React. A notable difference between HTML and JSX is that HTML requires a method like getElementById() in order to incorporate Javascript by linking the HTML file to an external Javascript file. JSX on the other hand can incorporate both HTML and Javascript in one file, making rendering to the DOM quicker.
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: Not totally sure on this one either.
 
-  Researched answer:
+  Researched answer: Yet Another Resource Negotiator (Yarn) is a package manager which can be used to automatically find, download, install, configure, upgrade, and uninstall a computer's packages. Yarn can be used to download and install libraries, plugins, etc. Yarn modifies yarn.lock and the README in a React application.
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: An anonymous function in Javascript is a function which has not been declared. It will return undefined.
 
-  Researched answer:
+  Researched answer: A Javascript anonymous function is a function without a name. Commonly assigned to a variable and passed as an argument to other functions.
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering: Rendering of only some of your React components to the DOM depending on the state of your application.
 
-2. Object-oriented programming:
+2. Object-oriented programming: A computer programming model which focuses software design around objects rather than functions and logic.
 
-3. Ruby:
+3. Ruby: A programming language which places a particular emphasis on programming simplicity through designating everything in the Ruby language as an object.
 
-4. Ruby blocks:
+4. Ruby blocks: Blocks are pieces of code in Ruby that function like methods but live outside of objects.
 
-5. Ruby hashes:
+5. Ruby hashes: A collection of key-value pairs in the Ruby programming language.
